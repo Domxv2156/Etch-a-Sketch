0822047ba4f3cba45e1af5f937fac34b05355a35
Dominik Kloc <dominic2156@hotmail.com>
2022-08-30 10:42:04 +0100

    add coloured option

--- java.js
+++ java.js
@@ -21,8 +21,32 @@ const square = document.createElement('div');
    }
    };
 };
+let numbers = ["blue", "red", "yellow", "green", "purple"]
+
+function randomColor() {
+   return numbers[Math.floor(Math.random() *numbers.length)]
+};
 
 
+
+function changeColor() {
+   let input = prompt("Choose your preferred grid size!")
+   if(input > 100 || input < 2) {
+    alert('The maximum and minimum numbers you can choose are 100 and 0.')
+} else {
+   board.style.gridTemplateColumns = `repeat(${input},1fr)`;
+   board.style.gridTemplateRows = `repeat(${input},1fr)`; 
+for(let i = 0; i<input*input; i++) {
+const square = document.createElement('div');
+    square.classList.add('grid');
+    board.appendChild(square);
+    square.addEventListener('dragenter', () => {
+     square.style.backgroundColor = randomColor();
+
+    })
+   };
+   }
+};
 
 
 
